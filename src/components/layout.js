c0786a36801e10a437fefaaf2df7c9e07196b534
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2020-11-01 22:51:56 +0500

    updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,18 +29,12 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 700,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        
       </div>
     </>
   )
